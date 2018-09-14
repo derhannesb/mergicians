6cb6023c59c5d3e5b1b2da156af04d23bb43a623
Johannes Bade <j.bade@conception.cc>
2018-09-15 01:12:45 +0200

    add ship trail

--- scenes/Ship.gd
+++ scenes/Ship.gd
@@ -12,4 +12,6 @@ func _physics_process(delta):
 		apply_impulse(Vector2(0,0), Vector2(0,10))
 	if Input.is_action_pressed("left"):
 		apply_impulse(Vector2(0,0), Vector2(-10,0))
-	
+	#$Trail/TrailParticles.lifetime = linear_velocity.length() / 20
+	#$Trail.rotation = position.angle_to(linear_velocity.normalized()) - PI/2 - rotation
+	#$Trail.rotation = - rotation
