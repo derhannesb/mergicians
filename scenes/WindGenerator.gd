b0489fdcbaace672724cef570478312732f3d8b1
Johannes Bade <j.bade@conception.cc>
2018-09-15 13:34:24 +0200

    add wind decay and basic storm

--- scenes/WindGenerator.gd
+++ scenes/WindGenerator.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
-export var strength = 0.01
+const strength_factor = 1250
+const decay_factor = 20
+
+export var energy = 100
+export var initial_strength = 0.01
+var strength = energy / strength_factor
+var energy_step = 0
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	if (energy > 0):
+		energy -= delta*decay_factor
+	if (energy < 0):
+		self.queue_free()
+	strength = energy / strength_factor
+	$Particles2D.modulate = Color(1,1,1,energy/100)
+
+	#$Particles2D.process_material.initial_velocity = energy
 	if $RayCast2D.is_colliding():
 		var collider = $RayCast2D.get_collider()
 		if (collider.is_in_group("ship")):
