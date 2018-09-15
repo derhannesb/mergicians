5df9bf1680be7e4c8cb54a3110f67add07e1f2a4
Simon Budig <simon@budig.de>
2018-09-15 03:19:54 +0200

    another go at island generation

--- scenes/Terrain.gd
+++ scenes/Terrain.gd
@@ -23,16 +23,63 @@ func generate_water():
 			self.add_child(water_part)
 
 func generate_islands():
-	for i in range(0,10):
+	for i in range(0,1):
 		generate_island(Vector2(rand_range(0,1920), rand_range(0,1080)))
 	
+func shuffleList(list):
+	var shuffledList = [] 
+	var indexList = range(list.size())
+	for i in range(list.size()):
+		var x = randi() % indexList.size()
+		shuffledList.append(list[indexList[x]])
+		indexList.remove(x)
+	return shuffledList
+
 func generate_island(var position):
-	var last_position = position
-	for a in range(0,rand_range(2,16)):
-		for i in range(0,rand_range(2,16)):
-			var island_part = pl_island_part.instance()
-			island_part.position = Vector2(last_position.x + rand_range(-20, 20) + (i*20),
-			                               last_position.y + rand_range(-20, 20) + (a*20))
-			island_part.rotation_degrees = rand_range(0,359)
-			self.add_child(island_part)
+	var dx = 128
+	var dy = 111
+	var elements = Dictionary()
+	
+	var start_position = position
+	
+	for a in range(0, 5 + rand_range (1, 16)):
+		position = start_position
+		for i in range (0, 10):
+			var dir = int (rand_range (0, 6))
+			if dir == 0:
+				position = Vector2 (position.x + dx, position.y)
+			elif dir == 1:
+				position = Vector2 (position.x + dx/2, position.y + dy)
+			elif dir == 2:
+				position = Vector2 (position.x - dx/2, position.y + dy)
+			elif dir == 3:
+				position = Vector2 (position.x - dx, position.y)
+			elif dir == 4:
+				position = Vector2 (position.x - dx/2, position.y - dy)
+			elif dir == 5:
+				position = Vector2 (position.x + dx/2, position.y - dy)
+				
+			var key = "" + str (int(position.x)) + "," + str(int(position.y))
+			if not elements.has (key):
+				elements[key] = Vector3 (position.x, position.y, 1)
+				break
+			else:
+				elements[key].z += 1
+				
+	var keys = elements.keys()
+	keys = shuffleList (keys)
+	
+	for key in keys:
+		var island_part = pl_island_part.instance()
+		if elements[key].z == 1:
+			island_part.get_node ("clay").show ()
+		elif elements[key].z < 3:
+			island_part.get_node ("rocks").show ()
+		else:
+			island_part.get_node ("grass").show ()
+			
+		island_part.position = Vector2 (elements[key].x + rand_range (-20, 20),
+		                                elements[key].y + rand_range (-20, 20))
+		island_part.rotation_degrees = rand_range(0,359)
+		self.add_child(island_part)
 	
